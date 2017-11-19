d745f7c47b5429fc0c3a3a8530d81b5482e1950c
Guanghui Wang <guanghui-wang@foxmail.com>
2017-11-19 22:15:59 +0800

    parent and child components communications

--- src/Input.vue
+++ src/Input.vue
@@ -1,14 +1,24 @@
+// 父组件传过来 msg 作为初始值。
+// 改变 input emit 事件并传递参数，父级订阅该事件并可以使用子级的参数
 <template>
   <div>
-    <input type="text" v-model="message">
+    <!-- <input type="text" v-model="message"> -->
+    <input type="text" v-bind:value="msg" v-on:input="changeMessage">
     <p>{{message}}</p>
   </div>
 </template>
 
 <script>
 export default {
+  props: ['msg'],
   data() {
     return {message : ''}
+  },
+  methods: {
+    changeMessage(event){
+      this.message = event.target.value;
+      this.$emit('messageChanged', this.message)
+    }
   }
 }
 </script>
--- src/Message.vue
+++ src/Message.vue
@@ -1,7 +1,10 @@
+// Message.vue 作为父级组件，要和 Input.vue 子组件通信
+// 父组件 → 子组件：父组件传递 msg 属性，子组件 props 接收  v-bind:msg="message"
+// 子组件 → 父组件：子组件 emit，父组件订阅事件 v-on:messageChanged="updateParentFromChild"
 <template>
   <div>
-    <h1>this is a message</h1>
-    <app-input></app-input>
+    <h1>{{message}}</h1>
+    <app-input v-bind:msg="message" v-on:messageChanged="updateParentFromChild"></app-input>
   </div>
 </template>
 
@@ -10,8 +13,18 @@
 import Input from './Input.vue'
 
 export default {
+  data() {
+    return {
+      message: 'this is the initial message'
+    }
+  },
   components: {
     'app-input': Input
+  },
+  methods: {
+    updateParentFromChild($event){
+      this.message = $event
+    }
   }
 }
 </script>
